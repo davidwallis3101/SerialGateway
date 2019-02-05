bbfde3ba02288dd44a274c36d8d0378fa4d76690
David Wallis <david@wallis2000.co.uk>
2019-02-05 22:36:13 +0000

    working code

--- SerialGateway/Program.cs
+++ SerialGateway/Program.cs
@@ -8,7 +8,8 @@ namespace SerialGateway
         static void Main()
         {
             //SerialHelper.GetAvailablePorts();
-            SerialHelper.SendCommand("/dev/ttyUSB0");
+            //SerialHelper.SendCommand("/dev/ttyUSB0");
+            SerialHelper.SendCommand("COM6");
             TcpHelper.StartServer(5678);
             TcpHelper.Listen();
         }
--- SerialGateway/SerialHelper.cs
+++ SerialGateway/SerialHelper.cs
@@ -24,22 +24,21 @@ namespace SerialGateway
                 sp.Encoding = Encoding.UTF8;
                 sp.BaudRate = 57600;
                 sp.Handshake = Handshake.XOnXOff;
+                //sp.Handshake = Handshake.None;
                 sp.DataBits = 8;
                 sp.Parity = Parity.None;
                 sp.StopBits = StopBits.One;
 
-                sp.ReadTimeout = 1000;
-                sp.WriteTimeout = 1000;
+                //sp.ReadTimeout = 1000;
+                //sp.WriteTimeout = 1000;
                 sp.Open();
-
+                int iter = 0;
                 bool finished = false;
 
                 while (!finished)
                 {
-                    //string line = Console.ReadLine();
-                    //if (line == "!q")
-                    //    break;
 
+   
                     //try
                     //{
                     //    sp.WriteLine(line);
@@ -49,34 +48,74 @@ namespace SerialGateway
                     //    Console.WriteLine("ERROR: Sending command timed out");
                     //}
 
-                    if (finished)
-                        break;
+                    //if (iter == 0)
+                    //{
+                        // Console.WriteLine("11 receive, send command");
+                        byte[] onCommand = new byte[] { 0x55, 0x04, 0xA0, 0x00, 0x07 };
+                        byte[] offCommand = new byte[] { 0x55, 0x04, 0xA1, 0x00, 0x06 };
+                    //for (int i = 0; i < 2; i++)
+                    //{
+                        Console.WriteLine($"Send on Command");
+                        //System.Threading.Thread.Sleep(500);
+                        sp.Write(onCommand, 0, onCommand.Length);
+
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine($"Send off Command");
+                        //System.Threading.Thread.Sleep(500);
+                        sp.Write(offCommand, 0, offCommand.Length);
+                        System.Threading.Thread.Sleep(1000);
+                    //}
+
+
+                    //iter++;
+                    //break;
+                    //}
+
+                    //if (finished)
+                    //    break;
 
                     // if RATE is set to really high Arduino may fail to respond in time
                     // then on the next command you might get an old message
-                    // ReadExisting will read everything from the internal buffer
-                    var existingData = sp.ReadExisting();
-                    Console.Write($"Existing Data: {existingData}");
+                    //ReadExisting will read everything from the internal buffer
+                    //var existingData = sp.ReadExisting();
+                    //Console.Write($"Existing Data: {existingData}\r\n");
 
 
                     //if (!existingData.Contains('\n') && !existingData.Contains('\r'))
                     //{
-                        // we didn't get the response yet, let's wait for it then
-                        try
-                        {
-                            int length = sp.BytesToRead;
-                            byte[] buf = new byte[length];
+                    // we didn't get the response yet, let's wait for it then
+                    //try
+                    //{
+                    //    int length = sp.BytesToRead;
+                    //    byte[] buf = new byte[length];
+                    //    sp.Read(buf, 0, length);
+                    //    if (length > 0)
+                    //    {
+                    //        //Console.WriteLine("Received Data: " + BitConverter.ToString(buf, 0, buf.Length));
 
-                            sp.Read(buf, 0, length);
-                            Console.WriteLine("Received Data: " + buf);
-                            Console.WriteLine("Received Data 2: " + BitConverter.ToString(buf, 0, buf.Length));
+                    //        if (buf[0] == 0x11)
+                    //        {
+                    //            Console.WriteLine("11 receive, send command");
+                    //            byte[] command = new byte[] { 0x55, 0x04, 0xA0, 0x00, 0x07 };
+                    //            sp.Write(command, 0, command.Length);
+                    //            // sp.Write(command, 0, command.Length);
 
-                            //Console.WriteLine($"ReadLine: {sp.ReadLine()}");
-                        }
-                        catch (TimeoutException)
-                        {
-                            Console.WriteLine($"ERROR: No response in {sp.ReadTimeout}ms.");
-                        }
+                    //            int respLength = sp.BytesToRead;
+                    //            byte[] respBuf = new byte[respLength];
+                    //            sp.Read(respBuf, 0, respLength);
+
+                    //            Console.WriteLine("Resp Data: " + BitConverter.ToString(respBuf, 0, respBuf.Length));
+
+                    //            break;
+                    //        }
+                    //    }
+
+
+                    //    //Console.WriteLine($"ReadLine: {sp.ReadLine()}");
+                    //}
+                    //catch (TimeoutException)
+                    //{
+                    //    Console.WriteLine($"ERROR: No response in {sp.ReadTimeout}ms.");
                     //}
                 }
             }
