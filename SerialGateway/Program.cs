be9da31e3aee1711334d620da301419627bdf446
David Wallis <david@wallis2000.co.uk>
2019-02-05 12:19:43 +0000

    Add project files.

---
+++ SerialGateway/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace SerialGateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SerialHelper.GetAvailablePorts();
+            SerialHelper.SendCommand("/dev/ttyUSB0");
+            TcpHelper.StartServer(5678);
+            TcpHelper.Listen();
+
+
+
+        }
+    }
+}
---
+++ SerialGateway/SerialHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using System.IO.Ports;
+
+namespace SerialGateway
+{
+    class SerialHelper
+    {
+        public static void GetAvailablePorts()
+        {
+            var ports = SerialPort.GetPortNames();
+            foreach (var port in ports)
+            {
+                Console.WriteLine(port);
+            }
+        }
+
+        public static bool SendCommand(string portName)
+        {
+
+
+            // to get port name you can use SerialPort.GetPortNames()
+            // string portName = "/dev/ttyUSB0";
+            int baudRate = 57600;
+
+
+
+
+
+            using (var sp = new SerialPort(portName))
+            {
+                sp.Encoding = Encoding.UTF8;
+                sp.BaudRate = baudRate;
+                sp.Handshake = Handshake.XOnXOff;
+                sp.DataBits = 8;
+                sp.Parity = Parity.None;
+                sp.StopBits = StopBits.One;
+
+                sp.ReadTimeout = 1000;
+                sp.WriteTimeout = 1000;
+                sp.Open();
+
+                bool finished = false;
+
+                while (!finished)
+                {
+                    //string line = Console.ReadLine();
+                    //if (line == "!q")
+                    //    break;
+
+                    //try
+                    //{
+                    //    sp.WriteLine(line);
+                    //}
+                    //catch (TimeoutException)
+                    //{
+                    //    Console.WriteLine("ERROR: Sending command timed out");
+                    //}
+
+                    if (finished)
+                        break;
+
+                    // if RATE is set to really high Arduino may fail to respond in time
+                    // then on the next command you might get an old message
+                    // ReadExisting will read everything from the internal buffer
+                    string existingData = sp.ReadExisting();
+                    Console.Write($"Existing Data:  {existingData}");
+
+
+                    if (!existingData.Contains('\n') && !existingData.Contains('\r'))
+                    {
+                        // we didn't get the response yet, let's wait for it then
+                        try
+                        {
+                            Console.WriteLine($"ReadLine: {sp.ReadLine()}");
+                        }
+                        catch (TimeoutException)
+                        {
+                            Console.WriteLine($"ERROR: No response in {sp.ReadTimeout}ms.");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
